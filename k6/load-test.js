b3c3680fe4596f482019fcb7f718776d3fbd7905
Felipe Schossler <felipealvesschossler@gmail.com>
2023-10-10 07:51:21 -0300

    feat: add fiber and structure better the repo

--- k6/load-test.js
+++ k6/load-test.js
@@ -1,10 +1,10 @@
 import http from 'k6/http';
 
 export const options = {
-  vus: 10, // Number of virtual users (simultaneous connections)
-  duration: '30s', // Test duration (e.g., 30 seconds)
+  vus: 100,
+  duration: '30s',
 };
 
 export default function () {
-  http.get('http://172.22.175.194:8080/hello');
+  http.get('http://api:8080/hello');
 }
